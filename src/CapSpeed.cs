ba9862947d7f4f0751cf12a54725b7199d978274
LordofNotCommitting <shadymcshade247@gmail.com>
2025-10-27 18:09:58 -0400

    first commit

---
+++ src/CapSpeed.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+namespace CapProductionTime
+{
+
+    [HarmonyPatch(typeof(ItemProductionSystem), nameof(ItemProductionSystem.StartMagnumItemProduction))]
+    public class CapSpeed
+    {
+        //steam mod ID 3594238447
+        static int cap_value = Plugin.ConfigGeneral.ModData.GetConfigValue<int>("Prod_Speed_Cap", 9999);
+
+        static bool Prefix(MagnumCargo magnumCargo, MagnumProjects projects, MagnumProgression magnumSpaceship, SpaceTime time, Difficulty difficulty, ItemProduceReceipt receipt, int count, int lineIndex)
+        {
+            
+            MagnumProject withModifications = projects.GetWithModifications(receipt.OutputItem);
+            float prodlineProduceSpeedBonus = magnumSpaceship.ProdlineProduceSpeedBonus;
+            int durationInHours = (int)Mathf.Max((receipt.ProduceTimeInHours + prodlineProduceSpeedBonus) * (1f / difficulty.Preset.MagnumCraftingTime), 1f) * (count > cap_value ? cap_value : count);
+            if (!magnumCargo.ItemProduceOrders.ContainsKey(lineIndex))
+            {
+                magnumCargo.ItemProduceOrders[lineIndex] = new List<ProduceOrder>();
+            }
+            ProduceOrder produceOrder = new ProduceOrder
+            {
+                OrderId = ((withModifications != null) ? withModifications.CustomRecord.Id : receipt.OutputItem),
+                Count = count,
+                DurationInHours = durationInHours,
+                StartTime = time.Time
+            };
+            foreach (ItemQuantity itemQuantity in receipt.RequiredItems)
+            {
+                for (int i = 0; i < itemQuantity.Count; i++)
+                {
+                    produceOrder.RequiredItems.Add(itemQuantity.ItemId);
+                }
+            }
+            magnumCargo.ItemProduceOrders[lineIndex].Add(produceOrder);
+            foreach (ItemQuantity itemQuantity2 in receipt.RequiredItems)
+            {
+                MagnumCargoSystem.RemoveSpecificCargo(magnumCargo, itemQuantity2.ItemId, (short)(count * itemQuantity2.Count));
+            }
+            return false;
+        }
+
+
+
+    }
+}
---
+++ src/ModConfigGeneral.cs
@@ -0,0 +1,31 @@
+﻿using MGSC;
+using ModConfigMenu.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CapProductionTime
+{
+    // Token: 0x02000006 RID: 6
+    public class ModConfigGeneral
+    {
+        // Token: 0x0600001D RID: 29 RVA: 0x00002840 File Offset: 0x00000A40
+        public ModConfigGeneral(string ModName, string ConfigPath)
+        {
+            this.ModName = ModName;
+            this.ModData = new ModConfigData(ConfigPath);
+            this.ModData.AddConfigHeader("STRING:General Settings", "general");
+            this.ModData.AddConfigValue("general", "about", "STRING:if you produce an item more than this 'Production Speed Cap', production time will be set as if you are making 'Production Speed Cap' number of items \n");
+            this.ModData.AddConfigValue("general", "Prod_Speed_Cap", 10, 1, 500, "STRING:Set Production Speed Cap", "STRING:Set the number of item where the production speed will cap at.");
+            this.ModData.AddConfigValue("general", "about2", "STRING:<color=#f51b1b>The game must be restarted after setting then saving this config to take effect.</color>\n");
+            this.ModData.RegisterModConfigData(ModName);
+        }
+
+        // Token: 0x04000011 RID: 17
+        private string ModName;
+
+        // Token: 0x04000012 RID: 18
+        public ModConfigData ModData;
+
+    }
+}
