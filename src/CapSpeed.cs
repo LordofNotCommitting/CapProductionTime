4325384b7479d8e609f22db9fb7dffad0cf894d2
LordofNotCommitting <shadymcshade247@gmail.com>
2025-10-28 23:35:29 -0400

    v1.1

--- src/CapSpeed.cs
+++ src/CapSpeed.cs
@@ -20,38 +20,18 @@ namespace CapProductionTime
         //steam mod ID 3594238447
         static int cap_value = Plugin.ConfigGeneral.ModData.GetConfigValue<int>("Prod_Speed_Cap", 9999);
 
-        static bool Prefix(MagnumCargo magnumCargo, MagnumProjects projects, MagnumProgression magnumSpaceship, SpaceTime time, Difficulty difficulty, ItemProduceReceipt receipt, int count, int lineIndex)
-        {
-            
-            MagnumProject withModifications = projects.GetWithModifications(receipt.OutputItem);
-            float prodlineProduceSpeedBonus = magnumSpaceship.ProdlineProduceSpeedBonus;
-            int durationInHours = (int)Mathf.Max((receipt.ProduceTimeInHours + prodlineProduceSpeedBonus) * (1f / difficulty.Preset.MagnumCraftingTime), 1f) * (count > cap_value ? cap_value : count);
-            if (!magnumCargo.ItemProduceOrders.ContainsKey(lineIndex))
-            {
-                magnumCargo.ItemProduceOrders[lineIndex] = new List<ProduceOrder>();
-            }
-            ProduceOrder produceOrder = new ProduceOrder
-            {
-                OrderId = ((withModifications != null) ? withModifications.CustomRecord.Id : receipt.OutputItem),
-                Count = count,
-                DurationInHours = durationInHours,
-                StartTime = time.Time
-            };
-            foreach (ItemQuantity itemQuantity in receipt.RequiredItems)
-            {
-                for (int i = 0; i < itemQuantity.Count; i++)
-                {
-                    produceOrder.RequiredItems.Add(itemQuantity.ItemId);
-                }
-            }
-            magnumCargo.ItemProduceOrders[lineIndex].Add(produceOrder);
-            foreach (ItemQuantity itemQuantity2 in receipt.RequiredItems)
-            {
-                MagnumCargoSystem.RemoveSpecificCargo(magnumCargo, itemQuantity2.ItemId, (short)(count * itemQuantity2.Count));
-            }
-            return false;
-        }
 
+
+        static void Postfix(MagnumCargo magnumCargo, MagnumProjects projects, MagnumProgression magnumSpaceship, SpaceTime time, Difficulty difficulty, ItemProduceReceipt receipt, int count, int lineIndex)
+        {
+
+            int time_cap = (count > cap_value ? cap_value : count);
+            float prodlineProduceSpeedBonus = magnumSpaceship.ProdlineProduceSpeedBonus;
+            int durationInHours_postfix = (int)Mathf.Max((receipt.ProduceTimeInHours + prodlineProduceSpeedBonus) * (1f / difficulty.Preset.MagnumCraftingTime), 1f) * time_cap;
+            //magnumCargo.ItemProduceOrders[lineIndex].Single().DurationInHours = durationInHours_postfix;
+            magnumCargo.ItemProduceOrders[lineIndex][magnumCargo.ItemProduceOrders[lineIndex].Count() - 1].DurationInHours = durationInHours_postfix;
+
+        }
 
 
     }
